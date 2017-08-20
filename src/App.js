94b8cc21e4b5ad630ef8349dc75220bf1805c641
Ian <ian.christopher.mason@gmail.com>
2017-08-20 13:18:31 -0500

    adding JSX

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'; 
 import './App.css';
 
-class App extends Component {
+class App extends Component { 
   render() {
+    var helloWorld = 'Welcome to the Road to learn React';
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      	<h2>{helloWorld}</h2>
       </div>
-    );
+    ); 
   }
 }
 
